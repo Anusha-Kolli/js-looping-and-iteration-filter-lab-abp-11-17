44f764cd8540ef0966cc5abf5a8d4b63f6c29d9b
Naga Anusha Kolli <kolli.anusha9@gmail.com>
2018-01-11 20:35:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,6 +13,6 @@ function fuzzyMatch(drivers , string) {
 
 function matchName(drivers, string) {
   return drivers.filter(function givenString(){
-    return  === string ;
+    return givenString.name === string ;
   })
 }
