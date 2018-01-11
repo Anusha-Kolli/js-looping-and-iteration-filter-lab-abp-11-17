c0bef4f2fa4a40115dcc7d10f8e096168cb8a8f2
Naga Anusha Kolli <kolli.anusha9@gmail.com>
2018-01-11 20:27:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution in this file
-function findMatching(drivers,collection) {
-  return drivers.filter(function(driverName) {
-    return driverName.toLowercase() === collection.toLowercase();
-  });
+function findMatching(drivers, string) {
+ return drivers.filter(function(el) {
+    return el.toLowerCase() === string.toLowerCase();
+  })
 }
