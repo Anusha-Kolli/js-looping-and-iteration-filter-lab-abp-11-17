e1551b8d1449dd9c40b248a599aa34766925d7a7
Naga Anusha Kolli <kolli.anusha9@gmail.com>
2018-01-11 20:22:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
 // Code your solution in this file
 function findMatching(drivers,collection){
-  for(const drivers of collection){
-    if(drivers[collection] === collection){
-      return drivers;
-    }
-  }
+  
 }
