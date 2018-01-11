22faaf3f13aedd5a119245bfd6816bd1cbb82222
Naga Anusha Kolli <kolli.anusha9@gmail.com>
2018-01-11 20:27:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution in this file
 function findMatching(drivers,collection){
-  return drivers.filter(function (driverName){
+  return drivers.filter(function(driverName){
     return driverName.toLowercase() === collection.toLowercase();
   });
 }
